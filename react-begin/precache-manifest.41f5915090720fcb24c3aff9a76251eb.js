28bd7a436b11f19fe2dd76f591a05a0cab6c60c2
Billy Chan <ccw.billy.123@gmail.com>
2019-12-28 19:52:27 +0800

    backup

---
+++ react-begin/precache-manifest.41f5915090720fcb24c3aff9a76251eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755623e4017999034d638c83bcff28a2",
+    "url": "/react-begin/index.html"
+  },
+  {
+    "revision": "5ff18f3bc957274ed0a0",
+    "url": "/react-begin/static/css/main.bf13b2b3.chunk.css"
+  },
+  {
+    "revision": "b8f02e00dc42513ccc07",
+    "url": "/react-begin/static/js/2.b244dbb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-begin/static/js/2.b244dbb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ff18f3bc957274ed0a0",
+    "url": "/react-begin/static/js/main.ac6b9608.chunk.js"
+  },
+  {
+    "revision": "13a4788523148687059e",
+    "url": "/react-begin/static/js/runtime-main.92d1cabd.js"
+  }
+]);
--- react-begin/service-worker.js
+++ react-begin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "billy1624.github.io/react-begin/precache-manifest.2212430d05d33459fb6834603a5c5d73.js"
+  "/react-begin/precache-manifest.41f5915090720fcb24c3aff9a76251eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("billy1624.github.io/react-begin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-begin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
